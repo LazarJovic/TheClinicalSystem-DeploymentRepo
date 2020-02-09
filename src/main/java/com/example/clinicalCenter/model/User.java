ed29f1ab3fb3b92d83641212fe157afe016c7b17
LazarJovic <lazar.13jovic@gmail.com>
2020-02-09 19:45:39 +0100

    Update User.java

--- src/main/java/com/example/clinicalCenter/model/User.java
+++ src/main/java/com/example/clinicalCenter/model/User.java
@@ -24,6 +24,7 @@ public class User implements UserDetails {
     @Column(name = "email")
     private String email;
 
+    @JsonIgnore
     @Column(name = "password")
     private String password;
 

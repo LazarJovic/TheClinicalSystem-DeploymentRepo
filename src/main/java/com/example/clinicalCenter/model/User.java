ec7aabb0d16e8aac9d1bf58353d2bef02583824b
LazarJovic <lazar.13jovic@gmail.com>
2020-02-09 19:18:18 +0100

    Update for deploy

--- src/main/java/com/example/clinicalCenter/model/User.java
+++ src/main/java/com/example/clinicalCenter/model/User.java
@@ -1,6 +1,6 @@
 package com.example.clinicalCenter.model;
 
-import net.minidev.json.annotate.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
